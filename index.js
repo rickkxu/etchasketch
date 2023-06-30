5c3dba29e7d1e91791b3fc659394541fa9d87506
Rick Xu <xuyy04@gmail.com>
2023-06-29 21:27:36 -0400

    Add Styling and Two More Buttons
    Implement color theme, fonts, button and slider styling. Create Erase
    and Rainbow button functionality.

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const board = document.querySelector(".board");
-const selectBtn = document.querySelector(".select");
+const slider = document.querySelector(".slider");
 const clearBtn = document.querySelector(".clear");
-
-let boardSize = 5;
+const sliderOutput = document.querySelector("h3");
+const eraseBtn = document.querySelector(".erase");
+const rainbowBtn = document.querySelector(".rainbow");
+let boardSize = 16;
+let brushStatus = "Regular";
+let rainbowArray = ["Yellow", "Green", "Red", "Blue", "Orange", "Purple"];
 
 setBoard();
 
-selectBtn.onclick = () => {
-    boardSize = prompt("Enter the desired number of squares per side:");
+slider.oninput = () => {
+    boardSize = slider.value;
     removeBoard();
     setBoard();
+    sliderOutput.textContent = `${slider.value} x ${slider.value}`;
 }
 
 clearBtn.onclick = () => {
@@ -25,13 +30,25 @@ function removeBoard(){
 function setBoard() {
     for (let i = 0; i < boardSize*boardSize; i++) {
         const square = document.createElement('div');
-        square.setAttribute("style", `box-sizing: border-box; border-width: 1px; border-style: solid; border-color: black; width: ${(1000/Math.abs(boardSize))}px; height: ${(1000/Math.abs(boardSize))}px;`)
+        square.setAttribute("style", `box-sizing: border-box; border-width: 1px; border-style: solid; border-color: black; width: ${(600/Math.abs(boardSize))}px; height: ${(600/Math.abs(boardSize))}px;`)
         square.setAttribute("class","square");
-        square.onmouseover = () => square.style.backgroundColor = "Black";
+        square.onmouseover = () => {
+            if (brushStatus == "Regular"){
+                square.style.backgroundColor = "Black";
+            } else if (brushStatus == "Erase"){
+                square.style.backgroundColor = "#FFFFF0";
+            } else if (brushStatus == "Rainbow"){
+                square.style.backgroundColor = rainbowArray[Math.round(Math.random()*rainbowArray.length)];
+            }
+        }
         board.appendChild(square);
     }
 }
 
+eraseBtn.onclick = () => {
+    brushStatus != "Erase" ? brushStatus = "Erase" : brushStatus = "Regular";
+}
 
-
-
+rainbowBtn.onclick = () => {
+    brushStatus != "Rainbow" ? brushStatus = "Rainbow" : brushStatus = "Regular";
+}
